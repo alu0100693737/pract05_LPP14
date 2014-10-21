b8fc8f4304139b17577223d5599aa5731ec8f84b
Fabián Díaz Lorenzo <alu0100702545@ull.edu.es>
2014-10-21 19:33:27 +0100

    empezando las pruebas

--- Rakefile
+++ Rakefile
@@ -1,6 +1,8 @@
 require "bundler/gem_tasks"
-task :default => spec
+
+
+task :default => :spec
 
 task :spec do
-	sh "rspec -I. -Ilib -Irspec rspec/preguntas/eleccion_simple_spec.rb
+	sh "rspec -I. -Ilib -Irspec rspec/preguntas/eleccion_simple_spec.rb"
 end
---
+++ rspec/preguntas/eleccion_simple_spec.rb
@@ -0,0 +1,22 @@
+
+
+
+module Preguntas
+	class EleccionSimple
+		describe Preguntas::EleccionSimple do
+			before :each do 
+				@q =Preguntas::EleccionSimple.new :pregunta => '2+2=',
+					:Op_correcta =>4,:Op_incorrecta =>[9,3,1]
+			end
+		
+			context "cuando se construye una pregunta" do
+				it " tiene que tener un texto y algunas opciones" do
+					expect(@q.pregunta)=='2+2='
+					expect(@q.Op_correcta)== 4
+					expect(@q.Op_incorrecta)==[9,3,1]
+				end
+			end
+		end
+	end
+end 
+
