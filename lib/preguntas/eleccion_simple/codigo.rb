6f4f9f012b0ea239d7a95d83633e56cc7556026f
Ivan Garcia <alu0100693737@ull.edu.es>
2014-10-22 15:22:15 +0200

    Añadida la parte de html a codigo.rb

--- lib/preguntas/eleccion_simple/codigo.rb
+++ lib/preguntas/eleccion_simple/codigo.rb
@@ -8,6 +8,19 @@ module Preguntas
       @Op_incorrecta = args[:Op_incorrecta]
       
     end
+    
+    def to_html
+		opcion = @Op_incorrecta+[@Op_correcta]
+		opcion = opcion.sample       
+		opcion= ''
+		opcion.each do |opcion|
+		opcion +=  %Q{<input type = "radio" value= "#{opcion}" name = 0 > #{opcion}\n}
+		html = <<-"HTML"
+		#{@pregunta}<br/>
+		#{opcion}
+		#HTML 
+		end
+    end
   end
 end
     
